26c7ac48b00ef66843b34ac0a835de790e2668ab
GomzaUlq <UlquiorraSin@yandex.ru>
2025-01-23 11:28:25 +0500

    Update cart.py

--- my_app/routers/cart.py
+++ my_app/routers/cart.py
@@ -5,7 +5,7 @@ from models import Cart, CartItem
 cart_bp = Blueprint('cart', __name__, template_folder='templates')
 
 
-# Создание новой корзины
+
 @cart_bp.route('/cart', methods=['POST'])
 def create_cart():
     data = request.get_json()
@@ -15,7 +15,6 @@ def create_cart():
     return jsonify({"message": "Корзина успешно создана!", "cart_id": new_cart.id}), 201
 
 
-# Получение информации о корзине
 @cart_bp.route('/cart/<int:cart_id>', methods=['GET'])
 def get_cart(cart_id):
     cart = Cart.query.get(cart_id)
@@ -31,15 +30,14 @@ def get_cart(cart_id):
 
 @cart_bp.route('/cart/<int:cart_id>/items', methods=['POST'])
 def add_item_to_cart(cart_id):
-    data = request.get_json()  # Получаем данные из JSON
-    product_id = data.get('product_id')  # Извлекаем product_id из данных
-    new_item = CartItem(cart_id=cart_id, product_id=product_id, quantity=1)  # Устанавливаем количество по умолчанию
+    data = request.get_json() 
+    product_id = data.get('product_id') 
+    new_item = CartItem(cart_id=cart_id, product_id=product_id, quantity=1)  
     db.session.add(new_item)
     db.session.commit()
     return jsonify({"message": "Товар успешно добавлен в корзину!"}), 201
 
 
-# Обновление количества товара в корзине
 @cart_bp.route('/cart/<int:cart_id>/items/<int:item_id>', methods=['PUT'])
 def update_cart_item(cart_id, item_id):
     data = request.get_json()
@@ -51,7 +49,7 @@ def update_cart_item(cart_id, item_id):
     return jsonify({"message": "Товар не найден в корзине"}), 404
 
 
-# Удаление товара из корзины
+
 @cart_bp.route('/cart/<int:cart_id>/items/<int:item_id>', methods=['DELETE'])
 def remove_cart_item(cart_id, item_id):
     item = CartItem.query.get(item_id)
@@ -62,7 +60,7 @@ def remove_cart_item(cart_id, item_id):
     return jsonify({"message": "Товар не найден в корзине"}), 404
 
 
-# Отображение корзины
+
 @cart_bp.route('/cart_view', methods=['GET'])
 def cart_view():
     return render_template('cart/cart_view.html')
