06e4cb9b9af09e46a770733369f1a31fd46dc59f
GomzaUlq <UlquiorraSin@yandex.ru>
2025-01-30 17:32:30 +0500

    Delete my_app directory

--- my_app/backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///./shop.db')
-    SECRET_KEY = os.environ.get("SECRET_KEY", "148u1hufw9h2r")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
